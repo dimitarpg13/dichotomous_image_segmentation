dd33047269716bf786f99c16d649f218a4f0ce96
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-05-25 22:04:41 -0400

    update manager

--- dichotomous_image_segmentation/segmentation_manager.py
+++ dichotomous_image_segmentation/segmentation_manager.py
@@ -11,19 +11,6 @@ import requests
 import matplotlib.pyplot as plt
 from io import BytesIO
 
-import numpy as np
-from PIL import Image
-import torch
-from torch.autograd import Variable
-from torchvision import transforms
-import torch.nn.functional as F
-import gdown
-import os
-
-import requests
-import matplotlib.pyplot as plt
-from io import BytesIO
-
 from IS_Net.data_loader_cache import normalize, im_reader, im_preprocess
 from IS_Net.models.isnet import ISNetGTEncoder, ISNetDIS
 
