bcffdb2d87cf9211b9eb1175c7e3adc9bdbf0e46
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-05-26 16:26:48 -0400

    update manager

--- dichotomous_image_segmentation/segmentation_manager.py
+++ dichotomous_image_segmentation/segmentation_manager.py
@@ -82,36 +82,49 @@ def build_model(hypar, device):
     return net
 
 
-def predict(net, inputs_val, shapes_val, hypar, device):
-    """
-    Given an Image, predict the mask
-    """
-    net.eval()
-
-    if hypar["model_digit"] == "full":
-        inputs_val = inputs_val.type(torch.FloatTensor)
-    else:
-        inputs_val = inputs_val.type(torch.HalfTensor)
-
-    inputs_val_v = Variable(inputs_val, requires_grad=False).to(device)  # wrap inputs in Variable
-
-    ds_val = net(inputs_val_v)[0]  # list of 6 results
-
-    pred_val = ds_val[0][0, :, :, :]  # B x 1 x H x W    # we want the first one which is the most accurate prediction
-
-    # recover the prediction spatial size to the orignal image size
-    pred_val = torch.squeeze(
-        F.upsample(torch.unsqueeze(pred_val, 0), (shapes_val[0][0], shapes_val[0][1]), mode='bilinear'))
-
-    ma = torch.max(pred_val)
-    mi = torch.min(pred_val)
-    pred_val = (pred_val - mi) / (ma - mi)  # max = 1
-
-    if device == 'cuda': torch.cuda.empty_cache()
-    return (pred_val.detach().cpu().numpy() * 255).astype(np.uint8)  # it is the mask we need
-
-
 class SegmentationManager:
-    def __init__(self):
-        pass
+    def __init__(self, device):
+        self.device = device
+
+        # parameters for inference
+        self.hypar = {"model_path": LOCAL_SAVED_MODELS_FOLDER,
+                      "restore_model": DIS_PRETRAINED_MODEL_FILE_NAME,
+                      "interm_sup": False,
+                      "model_digit": "full",
+                      "seed": 0,
+                      "cache_size": [1024, 1024],
+                      "input_size": [1024, 1024],
+                      "crop_size": [1024, 1024],
+                      "model": ISNetDIS()}  # parameters for inferencing
+
+        self.net = build_model(self.hypar, self.device)
+
+    def predict(self, inputs_val, shapes_val):
+        """
+        Given an Image, predict the mask
+        """
+        self.net.eval()
+
+        if self.hypar["model_digit"] == "full":
+            inputs_val = inputs_val.type(torch.FloatTensor)
+        else:
+            inputs_val = inputs_val.type(torch.HalfTensor)
+
+        inputs_val_v = Variable(inputs_val, requires_grad=False).to(self.device)  # wrap inputs in Variable
+
+        ds_val = net(inputs_val_v)[0]  # list of 6 results
+
+        pred_val = ds_val[0][0, :, :,
+                   :]  # B x 1 x H x W    # we want the first one which is the most accurate prediction
+
+        # recover the prediction spatial size to the orignal image size
+        pred_val = torch.squeeze(
+            F.upsample(torch.unsqueeze(pred_val, 0), (shapes_val[0][0], shapes_val[0][1]), mode='bilinear'))
+
+        ma = torch.max(pred_val)
+        mi = torch.min(pred_val)
+        pred_val = (pred_val - mi) / (ma - mi)  # max = 1
+
+        if device == 'cuda': torch.cuda.empty_cache()
+        return (pred_val.detach().cpu().numpy() * 255).astype(np.uint8)  # it is the mask we need
 
