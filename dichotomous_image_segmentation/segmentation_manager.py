30dbe1bf749e9c10e172a18f01823b98c24a6e0f
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-05-25 21:58:10 -0400

    update segmentation_manager

--- dichotomous_image_segmentation/segmentation_manager.py
+++ dichotomous_image_segmentation/segmentation_manager.py
@@ -39,6 +39,17 @@ LOCAL_SAVED_MODELS_FOLDER = "./saved_models"
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
+def maybe_download_pretrained_model():
+    """
+    Downloads the pretrained model and stored it locally if it doesn't exist.'
+    """
+    if not os.path.exists(LOCAL_SAVED_MODELS_FOLDER):
+        os.mkdir(LOCAL_SAVED_MODELS_FOLDER)
+
+        local_temp_file = hf_hub_download(repo_id=HUGGING_FACE_DIS_MODEL_REPO, filename=DIS_PRETRAINED_MODEL_FILE_NAME)
+        shutil.copy(local_temp_file, LOCAL_SAVED_MODELS_FOLDER)
+
+
 class SegmentationManager:
     def __init__(self):
         pass
