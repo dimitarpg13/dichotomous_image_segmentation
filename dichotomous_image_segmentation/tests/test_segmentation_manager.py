087d5cfe9c65a770b533b178e89f74a697ebe06d
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-05-27 10:50:00 -0400

    update unittest

--- dichotomous_image_segmentation/tests/test_segmentation_manager.py
+++ dichotomous_image_segmentation/tests/test_segmentation_manager.py
@@ -1,7 +1,11 @@
 import unittest
+from dichotomous_image_segmentation.segmentation_manager import SegmentationManager, DEVICE
 
 
 class SegmentationManagerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.seg_manager = SegmentationManager(DEVICE)
+
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
 
