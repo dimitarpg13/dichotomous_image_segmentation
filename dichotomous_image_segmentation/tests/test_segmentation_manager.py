06309246951f3074da5e8f9e431e0afd17455c08
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2024-05-27 12:02:51 -0400

    add unit test

--- dichotomous_image_segmentation/tests/test_segmentation_manager.py
+++ dichotomous_image_segmentation/tests/test_segmentation_manager.py
@@ -1,13 +1,23 @@
 import unittest
-from dichotomous_image_segmentation.segmentation_manager import SegmentationManager, DEVICE
+import os
+from pathlib import Path
+from dichotomous_image_segmentation.segmentation_manager import SegmentationManager, get_center_of_mass, DEVICE
+
+DIS_HOME = '/'.join(Path(os.path.abspath(os.path.dirname(__file__))).parts[:-2]).replace('/','',1)
+DATASET_PATH = 'demo_datasets/your_dataset/'
 
 
-class SegmentationManagerTestCase(unittest.TestCase):
+class TestSegmentationManager(unittest.TestCase):
     def setUp(self):
         self.seg_manager = SegmentationManager(DEVICE)
 
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+    def test_predict_from_file(self):
+        image_path = DIS_HOME + "/" + DATASET_PATH
+        image_name = 'smiling-man'
+        mask = self.seg_manager.predict_from_file(image_path, image_name, 'jpeg')
+        (x_coord_center_of_mass, y_coord_center_of_mass) = get_center_of_mass(mask)
+        self.assertEqual(x_coord_center_of_mass, 300)
+        self.assertEqual(y_coord_center_of_mass, 360)
 
 
 if __name__ == '__main__':
